839d9a78a0266d54c476429421c0068cad5e6078
Liam Benton <liambenton08@gmail.com>
2025-04-29 14:40:39 -0400

    Block Code Changed

--- BrickBreaker/Block.cs
+++ BrickBreaker/Block.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Xml;
+using System.Dynamic;
 
 namespace BrickBreaker
 {
@@ -16,6 +18,7 @@ namespace BrickBreaker
         public int y; 
         public int hp;
         public Color colour;
+        public int level = 1;
 
         public static Random rand = new Random();
 
@@ -26,5 +29,23 @@ namespace BrickBreaker
             hp = _hp;
             colour = _colour;
         }
+
+        //public void SetLevel()
+        //{
+        //    XmlReader reader = XmlReader.Create($"level{level}.xml");
+
+        //    while (reader.Read())
+        //    {
+        //        Block b = new Block();
+
+        //        reader.ReadToFollowing("brick");
+        //        b.x = Convert.ToInt16(reader.GetAttribute("x"));
+        //        b.y = Convert.ToInt16(reader.GetAttribute("y"));
+        //        b.hp = Convert.ToInt16(reader.GetAttribute("hp"));
+        //       // b.colour = Convert.ToColourreader.GetAttribute("colour")
+        //    }
+        //}
     }
+
+
 }
